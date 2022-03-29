9e12ffc149ac5b69240a0d5a911a4f3eefee7a7f
Kecskés László <h046702@stud.u-szeged.hu>
2022-03-29 20:41:13 +0200

    Blob kiíratás fix

--- ETR/tablak/tananyag.php
+++ ETR/tablak/tananyag.php
@@ -27,8 +27,14 @@ echo "<table border='1'>\n";
 while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
     echo "<tr>\n";
     foreach ($row as $item) {
-        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+        if(is_object($item)){
+            echo "    <td>" . "Ez egy Blob" . "</td>\n";
+        }else{
+            echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+        }
+
     }
+
     echo "</tr>\n";
 }
 echo "</table>\n";
